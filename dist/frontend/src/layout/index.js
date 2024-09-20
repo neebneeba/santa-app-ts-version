49e624e75e995bb981c2267783bf26ca201e3616
De Neba <neba0317@gmail.com>
2024-09-20 17:12:09 +0900

    new build for updated env

--- dist/frontend/src/layout/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// App's main layout for header footer background etc.
-const Layout = ({ children }) => {
-    return (<div className="py-5 h-screen bg-gradient-to-tr from-blue-300 to-blue-500">
-      {children}
-    </div>);
-};
-exports.default = Layout;
